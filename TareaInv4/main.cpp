21d4528d92f97f0b8c9c36093412e2e737f740e5
Samir <samir.suarez@utec.edu.pe>
2022-10-13 18:06:07 -0500

    Programa finalizado con colorines

--- TareaInv4/main.cpp
+++ TareaInv4/main.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include "Clave.h"
 #define NC "\e[0m"
-#define RED "\e[0;31m"
-#define GRN "\e[0;32m"
-#define CYN "\e[0;36m"
-#define REDB "\e[41m"
+
 using namespace std;
 
 int main()
 {
-    int n;
-    cout << "Numero de caracteres: ";
-    cin >> n;
-    Clave clavesecreta(n);
-    clavesecreta.set_binario();
-    clavesecreta.set_llave();
 
-    cout << "\nCadena original: "
-         << RED << clavesecreta.get_cadena() << NC;
-    cout << "\nBinario: "
-         << GRN << clavesecreta.get_binario() << NC;
-    cout << "\nLlave: "
-         << CYN << clavesecreta.get_llave() << NC;
+     int n;
+     cout << "Numero de caracteres: ";
+     cin >> n;
+     Clave clavesecreta(n);
+     clavesecreta.set_binario();
+     clavesecreta.set_llave();
+
+     cout << "\nCadena original: "
+          << "\e[0;93m" << clavesecreta.get_cadena() << NC;
+     cout << "\nBinario: "
+          << "\e[0;94m" << clavesecreta.get_binario() << NC;
+     cout << "\nLlave: "
+          << "\e[0;32m" << clavesecreta.get_llave() << NC;
 }
